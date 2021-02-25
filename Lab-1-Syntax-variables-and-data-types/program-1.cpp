10800326da8e747052d59a6502c47d3373ec8a97
Mohammad Nur <mdnur701@gmail.com>
2021-02-25 14:43:34 +0600

    programe 1 added Write a Program to calculate and display the volume of a CUBE having its height (h=10cm), width(w=12cm) and depth (8cm)

---
+++ Lab-1-Syntax-variables-and-data-types/program-1.cpp
@@ -0,0 +1,14 @@
+/**
+ * Write a Program to calculate and display the volume of a CUBE having its height (h=10cm), width(w=12cm) and depth (8cm)
+ */
+#include <iostream>
+using namespace std;
+
+int main(){
+    int h=10;
+    int w=12;
+    int depth =8;
+    int vol = h * w* depth; 
+    cout<< "volume is " << vol <<endl; 
+    return 0;
+}
