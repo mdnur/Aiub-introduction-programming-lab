1db819eb9b08d234e01080e29362ac49472ef2cf
Mohammad Nur <mdnur701@gmail.com>
2021-04-06 00:51:47 +0600

    lab-11.2 done

---
+++ Lab-11.2-File-Handling-using-File-Streams/program-1-create-file.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    fstream st;// Step 1: Creating object of fstream class
+    st.open("create.txt", ios::out); // Step 2: Creating new file
+    if (!st)// Step 3: Checking whether file exist
+    {
+        cout << "File creation failed";
+        cout << "File creation failed";
+    }
+    else
+    {
+        cout << "New file created";
+        st.close(); // Step 4: Closing file
+    }
+    return 0;
+}
